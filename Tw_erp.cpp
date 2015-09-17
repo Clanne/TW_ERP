98a79f6a44685f9810f0c01e91f6d2da9c7d1514
Emile Beretz <emile.beretz@gmail.com>
2015-09-17 09:55:25 +0100

    Recrute
    Recrutement

--- Tw_erp.cpp
+++ Tw_erp.cpp
@@ -1,6 +1,6 @@
 #include "Tw_erp.h"
 #include <iostream>
-#define EFF 0.8
+#static const float EFF = 0.8
 
 Commande::Commande(int num, std::string name, int dev, int gestion, int rendu)
 {
@@ -32,8 +32,8 @@ Entreprise::Entreprise()
     while (getline(projets,line))
     {
       pos = 0;
-      id =(int)(line.at(pos));
       pos = line.find(separator);
+      id = atoi(line.substr(0,pos).c_str());
       line.erase(0, pos + 1);
 
       pos = line.find(separator);
@@ -53,6 +53,7 @@ Entreprise::Entreprise()
 
       l_commandes.push_back(Commande(id,nom,dev,gestion,rendu));
     }
+    trier_commandes();
   }
   std::ifstream employes("employes.txt", std::ios::in);
   {
@@ -73,9 +74,22 @@ Entreprise::Entreprise()
       l_employes.push_back(Employe(job,code,gestion));
     }
   }
+  int w_jour_code, w_jour_gestion;
+  for (size_t i = 0; i < employes.size() ; i++)
+  {
+    w_jour_code = l_employes.at(i).code;
+    w_jour_gestion = l_employes.at(i).gestion;
+  }
+  for (size_t i = 0 ; i < 360 ; i++)
+  {
+      tabTravail[i][0] = w_jour_code;
+      tabTravail[i][1] = w_jour_gestion;
+  }
 }
 
-
+void Entreprise::trier_commandes(){
+	std::sort (l_commandes.begin(), l_commandes.end(), MonFoncteur());
+}
 
 void Entreprise::evaluer()
 {
@@ -86,50 +100,76 @@ void Entreprise::evaluer()
     Res tmp = faisable(l_commandes.at(i), temps_dev, temps_gestion);
     if ( !(tmp.doable_dev && tmp.doable_gestion) )
     {
-		
-	std::cout << l_commandes.at(i).id << "\tNom : " << l_commandes.at(i).nom << "\t NbDevreq : " << l_commandes.at(i).joursDev <<
-	 "\tNbGestreq : " << l_commandes.at(i).joursGestion << "\t NbJoursAvantRendu : " << l_commandes.at(i).joursRendu << "\t" << std::endl;
-	  std::cout << "COMMANDE INVALIDE\t nbJours fin developpement : " << (tmp.code + temps_dev) << "\t NbJours fin gestion : " << (tmp.gestion + temps_gestion) << std::endl;
-      OK = 1;
+      std::cout << l_commandes.at(i).id << "\tNom : " << l_commandes.at(i).nom << "\t NbDevreq : " << l_commandes.at(i).joursDev <<
+	    "\tNbGestreq : " << l_commandes.at(i).joursGestion << "\t NbJoursAvantRendu : " << l_commandes.at(i).joursRendu << "\t" << std::endl;
+	    std::cout << "COMMANDE INVALIDE\t nbJours fin developpement : " << (tmp.code + temps_dev) << "\t NbJours fin gestion : " << (tmp.gestion + temps_gestion) << std::endl;
+
+      float ratio_dev = (float)( (tmp.code + temps_dev) - l_commandes.at(i).joursRendu ) / (float)( l_commandes.at(i).joursRendu - temps_dev ); /*jours_dev_entrop / nbjour_dev_dispo*/
+      float ratio_gestion = (float)( (tmp.gestion + temps_gestion) - l_commandes.at(i).joursRendu ) / (float)( l_commandes.at(i).joursRendu - temps_gestion ); /*jours_dev_entrop / nbjour_dev_dispo*/
+
+      if ( ratio_dev > 1 || ratio_gestion == 0)
+      {
+        recruter(temps_dev - 60,1,0);
+      }
+      else if ( ratio_gestion > 1 || ratio_dev == 0)
+        {
+          recruter(temps_gestion - 60,0,1);
+        }
+        else
+          recruter(min(temps_dev,temps_gestion),0.5,0.5);
+      OK += 1;
+      i--;
     }
-    temps_dev += tmp.code;
-    temps_gestion += tmp.gestion;
+    else
+    {
+      temps_dev += tmp.code;
+      temps_gestion += tmp.gestion;
+    }
   }
-  if (!OK)
-    std::cout << "Quelle superbe entreprise, je voul'achête pour 1k de 1k d'€" << std::endl;
-  else
-    std::cout << "Impossible de livrer toutes les commandes" << std::endl;
+  std::cout << "Quelle superbe entreprise, je voul'achête pour 1k de 1k d'€, mais vous deverez engager " << OK << "emloyes pour remplir vos objectifs"<< std::endl;
 }
 
 
 Res Entreprise::faisable (Commande c, int debut_code, int debut_gestion)
 {
   float eff_code =0., eff_gestion=0.;
-  int temps_dev_dispo = c.joursRendu - debut_code;
-  int temps_gestion_dispo = c.joursRendu - debut_gestion;
-  
-  for (int i = 0; i < l_employes.size(); i++)
+
+  for (size_t j = debut_gestion; j < c.joursRendu; j++)
   {
-    eff_code    += l_employes.at(i).code;
-    eff_gestion += l_employes.at(i).gestion;
+    eff_gestion += tabTravail[j][1];
   }
-  
+  for (int i = debut_code; i < c.joursRendu; i++)
+  {
+    eff_code += tabTravail[j][0];
+  }
+
 	eff_code *= EFF;
 	eff_gestion *= EFF;
-	
+
   float tmp_dev_nec  =(float)c.joursDev / eff_code;
   float tmp_gest_nec =(float)c.joursGestion / eff_gestion;
-    
+
   bool faisable_code = (tmp_dev_nec < temps_dev_dispo);
   bool faisable_gestion = (tmp_gest_nec < temps_gestion_dispo);
-  
-  
+
+
   return (Res){tmp_dev_nec, tmp_gest_nec, faisable_code,faisable_gestion};
 }
 
 
-int Entreprise::recruter()
+int Entreprise::recruter(int j, float dev, float gest)
 {
+  for (size_t i = j+60; i < 20; i++)
+  {
+    tabTravail[i][0] += 0.5*dev;
+    tabTravail[i][1] += 0.5*gestion;
+  }
+  for ( i = j+80; i < 360; i++)
+  {
+    tabTravail[i][0] += 1*dev;
+    tabTravail[i][1] += 1*gestion;
+  }
+  std::cout << "Tu dois recruter qqn qui fait " << dev << " de dev et " << gest << " de gestion" << std::endl;
 }
 
 int main(int argc, char* argv[])
--- Tw_erp.h
+++ Tw_erp.h
@@ -8,34 +8,46 @@
 
 typedef struct res{int code; int gestion; bool doable_dev; bool doable_gestion;}Res;
 
+
 class Commande {
 	public :
 		Commande(int num, std::string name, int dev, int gestion, int rendu);
-	
+
 	  int id;
 	  std::string nom;
 	  int joursRendu;
 	  int joursDev;
 	  int joursGestion;
+		float tabTravail [360][2];
 };
 
 class Employe {
 	public :
 	  Employe(std::string post, int c, int g);
-	
+
 	  std::string job;
 	  float code;
 	  float gestion;
 };
 
+
+struct MonFoncteur
+{
+	bool operator() (Commande s1, Commande s2)
+	{
+		return s1.joursRendu < s2.joursRendu;
+	}
+};
+
+
 class Entreprise {
 	public :
 	  Entreprise();
-	  
+
 	  std::vector<Commande> l_commandes;
 	  std::vector<Employe> l_employes;
 	  void evaluer();
+		void trier_commandes();
 	  Res faisable (Commande c, int debut, int debut_gestion);
-	  int recruter();
+		int recruter(int j, float dev, float gest);
 };
-
